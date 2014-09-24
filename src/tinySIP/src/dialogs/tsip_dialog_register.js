3a1a3c3c6877fba2ae9282238c4b089994a95c99
Vytis Valentinavičius <vytis.valentinavicius@lamoda.ru>
2014-09-24 14:21:01 +0000

    CALLCENTER-3251: Reregister issue

--- src/tinySIP/src/dialogs/tsip_dialog_register.js
+++ src/tinySIP/src/dialogs/tsip_dialog_register.js
@@ -505,6 +505,9 @@ function __tsip_dialog_register_Any_2_InProgress_X_shutdown(ao_args) {
 function __tsip_dialog_register_Any_2_Terminated_X_transportError(ao_args) {
     var o_dialog = ao_args[0];
     o_dialog.signal(tsip_event_code_e.DIALOG_TRANSPORT_ERROR, "Transport error");
+
+    o_dialog.i_timerRefresh = 8000;
+    o_dialog.timer_schedule('register', 'Refresh');
     return 0;
 }
 
